b6e5fd8baf6ea7943c8ce1a066990fbf0b207083
YongJae_Kim <bluesink@bluesink.io>
2021-11-21 14:42:24 +0900

    sort lecture by date

--- lecture/models.py
+++ lecture/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from motivator.models import Motivator
 
 class Lecture(models.Model):
+	class Meta:
+		ordering = ['date']
+
 	title = models.CharField(max_length=70)
 	subtitle = models.CharField(max_length=70)
 	date = models.DateTimeField()

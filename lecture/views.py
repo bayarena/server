d7d3f0e17ae4349af562057c1cfb5321e3df2c8d
YongJae_Kim <bluesink@bluesink.io>
2021-11-21 17:23:41 +0900

    add lecture query

--- lecture/views.py
+++ lecture/views.py
@@ -7,3 +7,13 @@ class LectureViewSet(viewsets.ModelViewSet):
 	queryset = Lecture.objects.all()
 	serializer_class = LectureSerializer
 	permission_classes = [permissions.AllowAny] # For test
+
+	def get_queryset(self):
+		length = self.request.query_params.get('length')
+		if length is not None:
+			try:
+				length_int = int(length)
+				return Lecture.objects.all().order_by("-date")[0:length_int]
+			except ValueError:
+				pass
+		return Lecture.objects.all()
